15062277c15db6e0d2af5fb9b0b29ed3ecf0b08e
Shreyo Paul <shreyopaul403@gmail.com>
2024-06-10 16:52:34 +0530

    first commit

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
 })
 
 
-app.listen(PORT, function () {
+app.listen(process.env.PORT || PORT, function () {
     console.log(`Listening on port ${PORT}!`);
     connectDB()
 });
