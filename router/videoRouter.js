5410d7308b5be9ef4856327a13fc0cf6dfc0d2f0
Shreyo Paul <shreyopaul403@gmail.com>
2024-06-11 18:23:46 +0530

    first commit

--- router/videoRouter.js
+++ router/videoRouter.js
@@ -41,7 +41,7 @@ const upload = multer({ storage: storage });
 router.get("/test", (req, res) => {
     const { db } = mongoose.connection;
     console.log(db)
-    res.send("Hello user! Warm wishings! Test")
+    res.send("Hello user! Warm wishings! Test", db)
 })
 
 router.post('/upload', authenticateToken, upload.fields([{ name: 'file' }, { name: 'thumbnail' }]), async function (req, res) {
